ba023fcea20ce19b38c87121027a81d5986eed14
Ulisse Mini <ulisse.mini@gmail.com>
2022-02-07 17:25:37 -0500

    Merge updates in UlisseMini/oth

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ import remarkMath from "remark-math";
 import remarkWikiLink from "remark-wiki-link";
 import klaw from "klaw";
 import path from "path";
-import { mkdir } from "fs/promises";
+import fs from "fs-extra";
+import frontmatter from "front-matter";
 
 main();
 
@@ -20,31 +21,42 @@ async function main() {
       const sourceVFile = await read(file.path);
       const htmlVFile = await compile(sourceVFile);
 
-      // FIXME: Make dirname robust to paths like my-notes/notes
-      htmlVFile.dirname = sourceVFile.dirname.replace(/notes/, "out");
+      htmlVFile.dirname = notesToOutPath(sourceVFile.dirname);
       htmlVFile.extname = ".html";
       htmlVFile.stem = pageResolver(sourceVFile.stem);
 
-      await mkdir(htmlVFile.dirname, { recursive: true });
+      await fs.mkdir(htmlVFile.dirname, { recursive: true });
       await write(htmlVFile);
       console.log(`wrote ${htmlVFile.path}`);
+    } else {
+      if (!file.stats.isDirectory() && !file.path.includes(".obsidian")) {
+        await copy(file.path, notesToOutPath(file.path));
+      }
     }
   }
+
+  await copy("node_modules/katex/dist/katex.min.css", "out/katex.min.css");
+  await copy("node_modules/katex/dist/fonts", "out/fonts");
+  await copy(
+    "node_modules/highlight.js/styles/default.css",
+    "out/highlight.css"
+  );
 }
 
-const pageResolver = (name) => name.toLowerCase().replace(/ /g, "-");
-const depth = (file) =>
-  file.dirname
-    .split("/")
-    .reverse()
-    .findIndex((p) => p === "notes");
-
 async function compile(file) {
+  const fm = frontmatter(file.value.toString());
+  file.value = fm.body;
+
+  // Relative path to root, needed to handle the root being user.github.io/project
+  const depth = file.path.split("/").reverse().lastIndexOf("notes");
+  const root = "../".repeat(depth);
+
   return await unified()
     .use(remarkParse)
     .use(remarkWikiLink, {
       // This pkg is yucky, the defaults are wierd. it's only ~30 lines though, the parsing
       // code is done here: https://github.com/landakram/micromark-extension-wiki-link
+      aliasDivider: "|",
       pageResolver: (name) => [pageResolver(name)],
       hrefTemplate: (permalink) => permalink,
     })
@@ -53,14 +65,25 @@ async function compile(file) {
     .use(rehypeHighlight)
     .use(rehypeKatex)
     .use(rehypeDocument, {
-      title: file.stem,
+      title: fm.attributes.title || file.stem,
       // Could fetch @latest css, but I'm afraid of breaking changes (eg. class name changes)
       css: [
-        "https://cdn.jsdelivr.net/npm/katex@0.15.0/dist/katex.min.css",
-        "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.4.0/build/styles/default.min.css",
-        "../".repeat(depth(file)) + "styles.css",
+        root + "styles.css",
+        root + "highlight.css",
+        root + "katex.min.css",
       ],
     })
     .use(rehypeStringify)
     .process(file);
+}
+
+// convert "Hello World.md" -> hello-world.md
+const pageResolver = (name) => name.toLowerCase().replace(/ /g, "-");
+
+// convert a/b/notes/c/d -> a/b/out/c/d
+const notesToOutPath = (p) => path.join("out", path.relative("notes", p));
+
+async function copy(src, dst) {
+  await fs.copy(src, dst);
+  console.log(`copied ${src} -> ${dst}`);
 }
