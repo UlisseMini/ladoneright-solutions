ab1f88fefc1de324cc513a78bd77eedaf69f3c62
Ulisse Mini <ulisse.mini@gmail.com>
2022-02-12 15:29:59 -0500

    Deduplicate headers, generalize dynamic notes

--- main.js
+++ main.js
@@ -20,21 +20,23 @@ import fs from "fs-extra";
 import frontmatter from "front-matter";
 import {reporter} from "vfile-reporter";
 import {visit} from "unist-util-visit";
+import {VFile} from 'vfile';
 
 main();
 
 async function main() {
   for await (const file of klaw("./notes")) {
-    if (path.extname(file.path) === ".md") {
-      const sourceVFile = await read(file.path);
-      const htmlVFile = await compile(sourceVFile);
-
-      htmlVFile.dirname = notesToOutPath(sourceVFile.dirname);
-      htmlVFile.extname = ".html";
-      htmlVFile.stem = pageResolver(sourceVFile.stem);
-
-      await fs.mkdir(htmlVFile.dirname, {recursive: true});
-      await write(htmlVFile);
+    const ext = path.extname(file.path)
+    if (ext === ".md") {
+      const markdownVFile = await read(file.path);
+      await compileAndWrite(markdownVFile);
+    } else if (ext === ".js") {
+      // NOTE: The smartest possible thing would be to run js code blocks in
+      // notes if they're annotated with "// :run:", this is a bit complicated
+      // to add, but is the nicest way to do this without leaving obsidian.
+      const markdown = (await import(file.path)).markdown;
+      const markdownVFile = new VFile({path: file.path, value: markdown});
+      await compileAndWrite(markdownVFile);
     } else {
       if (!file.stats.isDirectory() && !file.path.includes(".obsidian")) {
         await copy(file.path, notesToOutPath(file.path));
@@ -48,11 +50,6 @@ async function main() {
     "node_modules/highlight.js/styles/default.css",
     "out/highlight.css"
   );
-
-  // A general solution would be to check for index.js, then pass it the content from index.md
-  // this is quicker though.
-  const indexHTML = (await import("./notes/index.js")).html;
-  await fs.writeFile("out/index.html", indexHTML);
 }
 
 async function compile(file) {
@@ -70,6 +67,11 @@ async function compile(file) {
       ? relativePath.replace(solutionRegex, "Solution $1 $2 to Axler's Linear Algebra Done Right")
       : "Solutions to Axler's Linear Algebra Done Right"
   }
+  const title = fm.attributes.title || (
+    solutionRegex.test(relativePath)
+      ? relativePath.replace(solutionRegex, "Solution $1 $2")
+      : file.stem
+  )
 
   return await unified()
     .use(remarkParse)
@@ -86,25 +88,39 @@ async function compile(file) {
     .use(remarkValidateLinks)
     // uncomment if you want to spellcheck
     // .use(remarkRetext, unified().use(retextEnglish).use(retextSpell, dictionary))
-    .use(remarkRehype)
+    .use(remarkRehype, {allowDangerousHtml: true})
     .use(rehypeHighlight)
     .use(rehypeKatex)
     .use(rehypeDocument, {
-      title: fm.attributes.title || file.stem,
+      title: title,
       meta: meta,
+
       // Could fetch @latest css, but I'm afraid of breaking changes (eg. class name changes)
       css: [
         root + "styles.css",
         root + "highlight.css",
         root + "katex.min.css",
       ],
+
+      // TODO: Use a rehype-head plugin or something to add this
+      // <script async defer data-domain="ulissemini.github.io" src="https://a.nerdsniper.net/a.js"></script>
     })
-    .use(rehypeStringify)
+    .use(rehypeStringify, {allowDangerousHtml: true})
     .process(file)
     .then((file) => {
       process.stderr.write(reporter(file, {quiet: true}))
       return file
     })
+}
+
+async function compileAndWrite(markdownVFile) {
+  const htmlVFile = await compile(markdownVFile)
+  htmlVFile.dirname = notesToOutPath(markdownVFile.dirname);
+  htmlVFile.extname = ".html";
+  htmlVFile.stem = pageResolver(markdownVFile.stem);
+
+  await fs.mkdir(htmlVFile.dirname, {recursive: true});
+  await write(htmlVFile);
 }
 
 function remarkNoInlineDoubleDollar() {
@@ -130,7 +146,6 @@ const pageResolver = (name) => name.toLowerCase().replace(/ /g, "-");
 
 // convert a/b/notes/c/d -> a/b/out/c/d
 const notesToOutPath = (p) => path.join("out", path.relative("notes", p));
-
 
 async function copy(src, dst) {
   await fs.copy(src, dst);
--- notes/index.js
+++ notes/index.js
@@ -5,25 +5,15 @@ const completedListHTML = glob
   .sync("notes/*/*.md")
   .map((p) => path.relative("notes", p.replace(/\.md$/, "")))
   .filter((p) => p.match(/\d\w\/\d+/))
-  .map((p) => `<li><a href=\"${p}\">${p}</a></li>`)
+  .map((p) => `<li><a href="${p}">${p}</a></li>`)
   .join("\n");
 
-export const html = String.raw`
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Index</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="styles.css">
-<link rel="stylesheet" href="highlight.css">
-<link rel="stylesheet" href="katex.min.css">
-</head>
-<body>
-<p>These are my solutions to axler's linear algebra done right, to view a specific solution go to <code>/section/number</code>, eg. to view my solution to exercise <code>1</code> in section <code>2.A</code> go to <code>/2a/1</code>.</p>
+export const markdown = `
+# Linear Algebra Done Right Solutions
+
+These are my solutions to axler's linear algebra done right, to view a specific solution go to \`/section/number\`, eg. to view my solution to exercise \`1\` in section \`2.A\` go to \`/2a/1\`.
+
 <ul class="column-count">
 ${completedListHTML}
 </ul>
-</body>
-</html>
 `;
