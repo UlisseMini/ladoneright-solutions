d46f99365479056ee19ff13e05641cb229dcc15c
Ulisse Mini <ulisse.mini@gmail.com>
2022-02-15 16:53:40 -0500

    Fix sort and coloring

--- notes/index.js
+++ notes/index.js
@@ -2,25 +2,29 @@ import glob from "glob";
 import path from "path";
 import fs from "fs-extra";
 
-// TODO: make async
-const files = glob.sync("notes/*/*.md");
-
-const classes = await Promise.all(
-  files.map(async (file) =>
-    /todo/i.test(await fs.readFile(file)) ? "todo" : "not-todo"
-  )
-);
-
 // convert 3b/12 -> 3000 + 200 + 12 for sorting
 const f = (x) =>
   parseInt(x[0]) * 1000 +
   (x.charCodeAt(1) - "a".charCodeAt(0)) * 100 +
   parseInt(x.slice(3));
 
+// TODO: make async
+const files = glob
+  .sync("notes/*/*.md")
+  .map((p) => path.relative("notes", p))
+  .filter((p) => p.match(/\d\w\/\d+\.md$/))
+  .sort((a, b) => f(a) - f(b));
+
+const classes = await Promise.all(
+  files.map(async (file) =>
+    /todo/i.test(await fs.readFile(path.join("notes", file)))
+      ? "todo"
+      : "not-todo"
+  )
+);
+
 const completedListHTML = files
-  .map((p) => path.relative("notes", p.replace(/\.md$/, "")))
-  .filter((p) => p.match(/\d\w\/\d+/))
-  .sort((a, b) => f(a) - f(b))
+  .map((p) => p.replace(/\.md$/, ""))
   .map((p, i) => `<li class=\"${classes[i]}\"><a href=\"${p}\">${p}</a></li>`)
   .join("\n");
 
