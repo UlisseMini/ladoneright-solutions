f1f201920ea90d417158a504ad9b2fe0d5e9da56
Ulisse Mini <ulisse.mini@gmail.com>
2022-02-15 19:59:11 -0500

    Inline color styles in notes/index.js

--- notes/index.js
+++ notes/index.js
@@ -15,17 +15,18 @@ const files = glob
   .filter((p) => p.match(/\d\w\/\d+\.md$/))
   .sort((a, b) => f(a) - f(b));
 
-const classes = await Promise.all(
-  files.map(async (file) =>
-    /todo/i.test(await fs.readFile(path.join("notes", file)))
-      ? "todo"
-      : "not-todo"
-  )
+const isTodo = await Promise.all(
+  files.map(async (p) => /todo/i.test(await fs.readFile(path.join("notes", p))))
 );
 
 const completedListHTML = files
   .map((p) => p.replace(/\.md$/, ""))
-  .map((p, i) => `<li class=\"${classes[i]}\"><a href=\"${p}\">${p}</a></li>`)
+  .map(
+    (p, i) =>
+      `<li><a ${
+        isTodo[i] ? ' style="color: red;"' : ""
+      } href=\"${p}\">${p}</a></li>`
+  )
   .join("\n");
 
 export const markdown = `
@@ -36,6 +37,8 @@ title: "Linear Algebra Done Right Solutions"
 # Linear Algebra Done Right Solutions
 
 These are my solutions to axler's linear algebra done right, to view a specific solution go to \`/section/number\`, eg. to view my solution to exercise \`1\` in section \`2.A\` go to \`/2a/1\`.
+
+Unfinished solutions are colored <span style="color: red;">red</span>.
 
 <ul class="column-count">
 ${completedListHTML}
